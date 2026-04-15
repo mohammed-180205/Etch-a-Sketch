9bdc5e63bb11a3f10b0ee3e71f98560bec9f248e
mohammed-180205 <mohammed.180205@gmail.com>
2026-04-15 16:58:34 +0300

    applying DOM concepts to change the grid based on user input

--- main.js
+++ main.js
@@ -9,6 +9,27 @@ square.addEventListener("mouseenter", ()=> {square.style.backgroundColor = "blac
 }
 
 
+let btn = document.querySelector(".button")
+btn.addEventListener("click", () => {
+    let num = prompt("type a number (max of 100)");
+    
+    if(num < 0 || num > 100){
+        alert("type a number between 1-100");
+    } else if(num > 0 || num < 100){
+        container.innerHTML = "";
+        for(let i = 1; i <= num**2; i++){
+let userSquare = document.createElement("div");
+let userSquareWidth = (1000/num - 2) + "px";
+userSquare.style.width = userSquareWidth;
+userSquare.style.height = userSquareWidth;
+userSquare.classList.add("userSquare");
+container.appendChild(userSquare);
+userSquare.addEventListener("mouseenter", ()=> {userSquare.style.backgroundColor = "black"});
 
-square.addEventListener("mouseenter", changeColor);
+}
+
+    } else{
+        alert("type a valid number betweeen 1-100");
+    }
+});
 
