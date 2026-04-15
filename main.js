d43c35131276a7359b52ccad9f08821e05c7f172
mohammed-180205 <mohammed.180205@gmail.com>
2026-04-15 12:17:14 +0300

    creating the square using a loop

---
+++ main.js
@@ -0,0 +1,9 @@
+let container = document.querySelector(".container");
+
+for(let i = 1; i <= 256; i++){
+let square = document.createElement("div");
+square.classList.add("square");
+container.appendChild(square);
+
+}
+
