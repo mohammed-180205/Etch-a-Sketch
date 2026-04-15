fe24f020146f2452bbd0e7de1bb6c55d64c5808d
mohammed-180205 <mohammed.180205@gmail.com>
2026-04-15 12:54:11 +0300

    setting up a hover effect

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ for(let i = 1; i <= 256; i++){
 let square = document.createElement("div");
 square.classList.add("square");
 container.appendChild(square);
+square.addEventListener("mouseenter", ()=> {square.style.backgroundColor = "black"});
 
 }
+
+
+
+square.addEventListener("mouseenter", changeColor);
 
